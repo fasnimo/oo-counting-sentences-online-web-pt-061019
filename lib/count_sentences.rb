58a82059b95b82b76cf4e35d673cba5104fbac4e
Rene Cocom <fasnimo@gmail.com>
2019-07-08 23:35:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,8 @@ class String
   end
 
   def count_sentences
-      self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
-        self.split(/[.!?]+/)
+      
+        self.split(/[.!?]+/).count
+        return
     end
 end
